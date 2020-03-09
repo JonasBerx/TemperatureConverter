409928f1c1e7e4fa839d3f2891b3bed11fc0b35f
JonasBerx <jonas.berx1@student.ucll.be>
2020-03-09 12:39:50 +0100

    a

--- TemperatureConverter/View/MainWindow.xaml.cs
+++ TemperatureConverter/View/MainWindow.xaml.cs
@@ -26,70 +26,35 @@ namespace View
             InitializeComponent();
         }
 
-        private void ConvertCelsius(object sender, RoutedEventArgs e)
-        {
-            var cels = double.Parse(celsiusTextBox.Text);
-
-
-            var fahr = (cels * 9 / 5) + 32;
-            var kelv = cels + 273.15;
-
-            fahrenheitTextBox.Text = fahr.ToString();
-            kelvinTextBox.Text = kelv.ToString();
-            
-            
-        }
-        private void ConvertFahrenheit(object sender, RoutedEventArgs e)
-        {
-            var fahr = double.Parse(fahrenheitTextBox.Text);
-            var cels = (fahr - 32) * 5 / 9;
-            var kelv = (fahr + 459.67) * 5 / 9;
-
-            celsiusTextBox.Text = cels.ToString();
-            kelvinTextBox.Text = kelv.ToString();
-        }
-        private void ConvertKelvin(object sender, RoutedEventArgs e)
-        {
-            var kelv = double.Parse(kelvinTextBox.Text);
-            var cels = kelv - 273.15;
-            var fahr = (kelv - 273.15) * 9 / 5 + 32;
-
-            celsiusTextBox.Text = cels.ToString();
-            fahrenheitTextBox.Text = fahr.ToString();
-        }
-
         private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            var value = sliderValue.Value; //In kelvin scale
+            var kelvin = slider.Value;
+            var celsius = kelvin - 273.15;
+            var fahrenheit = celsius * 1.8 + 32;
 
+            var kelvinString = kelvin.ToString();
+            var fahrenheitString = fahrenheit.ToString();
 
-            
-            var fahr = (value - 273.15) * 9 / 5 + 32;
-            
-            fahrenheitTextBox.Text = fahr.ToString();
+            fahrenheitTextBox.Text = fahrenheitString;
         }
-
-        public class CelsiusConverter : IValueConverter
-        {
-            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                var kelvin = (double)value;
-                var celsius = kelvin - 273.15;
-
-                
-                return celsius.ToString();
-
-            }
-
-            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-            {
-                var celsius = double.Parse((string)value);
-                var kelvin = celsius + 273.15;
-
-                return kelvin;
-            }
-        }
-
     }
-    
+
+    public class CelsiusConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var kelvin = (double)value;
+            var celsius = kelvin - 273.15;
+
+            return celsius;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var celsius = double.Parse((string)value);
+            var kelvin = celsius + 273.15;
+
+            return kelvin;
+        }
+    }
 }
