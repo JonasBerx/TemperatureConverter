a94208fb28b817aca9db73f74569fd92978177b1
JonasBerx <jonas.berx1@student.ucll.be>
2020-03-18 09:39:47 +0100

    temp scales

---
+++ TemperatureConverter/View/CelsiusTemperatureScale.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    class CelsiusTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Celsius";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            var celsius = temperature - 273.15;
+            return celsius;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            var kelvin = temperature + 273.15;
+            return kelvin;
+        }
+    }
+}
---
+++ TemperatureConverter/View/FahrenheitTemperatureScale.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace View
+{
+    class FahrenheitTemperatureScale : ITemperatureScale
+    {
+        public string Name => "Fahrenheit";
+
+        public double ConvertFromKelvin(double temperature)
+        {
+            var fahrenheit = temperature * 1.8 - 459.67;
+            return fahrenheit;
+        }
+
+        public double ConvertToKelvin(double temperature)
+        {
+            var kelvin = (temperature + 459.67) / 1.8;
+            return kelvin;
+
+        }
+    }
+}
